3e2945ed6e42c1770d396955468002ca5cc822b4
Jason Lengstorf <jason@lengstorf.com>
2018-09-24 13:20:57 -0700

    fix: add headers to the response. I think.

--- src/handleFormSubmission/index.js
+++ src/handleFormSubmission/index.js
@@ -2,5 +2,14 @@ exports.handler = async message => {
   console.log('Hello!');
   console.log(message);
 
-  return {};
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers':
+        'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: { message: 'Everything is awesome!' }
+  };
 };
